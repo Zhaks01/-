9dea7a1611f7d3162e8493e0f527a1301f011e06
Zhaks01 <73010238+Zhaks01@users.noreply.github.com>
2021-09-30 15:07:28 +0600

    Create js.js

---
+++ pr2,2/js.js
@@ -0,0 +1,35 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+private: false
+};
+
+
+for (let i = 0; i < 1; i++) {
+const a = prompt('Один из последних просмотренных фильмов?'),
+b = prompt('На сколько оцените его?');
+
+if(a == '' || b == '' || a == null || b == null || a.length > 50) {
+console.log('error');
+i--; //
+continue; //
+}
+console.log('done');
+personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+console.log('Вы киноман');
+} else {
+console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
